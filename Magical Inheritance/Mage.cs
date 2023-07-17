5c496a97aa5660874d470e5d18559bd872a20e28
tasimm <taylorasimmons@gmail.com>
2023-07-17 07:44:20 -0400

    Create Mage.cs

---
+++ Magical Inheritance/Mage.cs
@@ -0,0 +1,20 @@
+// Mage class
+
+using System;
+
+namespace MagicalInheritance
+{
+  class Mage : Pupil
+  {
+    public Mage(string title) : base(title)
+    {}
+
+    public Mage(string title, string origin) : base(title, origin)
+    {}
+
+    public virtual Storm CastRainStorm()
+    {
+      return new Storm("rain", Origin, false, Title);
+    }
+  }
+}
